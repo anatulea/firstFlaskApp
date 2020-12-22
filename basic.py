07bf728b621d4d9ef21150dcae44e99875e4341d
Ana Tulea <anatulea14@gmail.com>
2020-12-21 22:20:11 -0800

    Flask Migrate

--- basic.py
+++ basic.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -13,6 +14,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app) # pass the application to the SQLAlchemy class
  
+Migrate(app, db)
+'''
+How to run migrations in terminal :
+>> export FLASK_APP=basic.py
+>> flask db init
+>> flask db migrate -m "some message"
+>> flask db upgrade
+
+'''
+
+
  ###########################
 class Puppy(db.Model):
       # manual table name choice:
@@ -25,10 +37,13 @@ class Puppy(db.Model):
 
     age = db.Column(db.Integer)
 
+    breed = db.Column(db.Text)
 
-    def __init__(self, name, age):
+
+    def __init__(self, name, age, breed):
         self.name = name
         self. age = age
+        self. breed = breed
 
     def __repr__(self):
         return f"Puppy {self.name} is {self.age} years old."
